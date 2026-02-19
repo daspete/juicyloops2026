cf5c2eb91d27e2032bf81fa21144d0c54bc6ad1f
daspete <daspetemail@gmail.com>
2026-02-19 18:06:54 +0100

    feat: added analytics

--- juicyloops/frontend/src/main.ts
+++ juicyloops/frontend/src/main.ts
@@ -3,10 +3,25 @@ import { createApp } from 'vue';
 import { router } from '@/router';
 import { theme } from '@/theme';
 import App from './app.vue';
+import { createPlausible } from 'v-plausible/vue';
 
 const app = createApp(App);
 
+const plausible = createPlausible({
+    init: {
+        domain: 'juicyloops.daspete.at',
+        apiHost: 'https://analytics.dev.alpenstudios.com',
+        trackLocalhost: true,
+    },
+    settings: {
+        enableAutoPageviews: true,
+        enableAutoOutboundTracking: true,
+    },
+});
+
 router(app);
 theme(app);
+
+app.use(plausible);
 
 app.mount('#app');
